40faf867376f77a122a9cc1b5a9ad4499b43ff65
Zaurtime <zaurteam@gmail.com>
2023-01-11 14:25:05 +0000

    Add js questions and var

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,3 +3,19 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull= document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswer = true
+let score = 0 
+let quistionCounter = 0
+let availableQuestions = []
+
+let questions = [ 
+    {
+        question: 'What is the most difficult IronMan?'
+        choice1: 'Patagonman, Chile'
+        choice2: 'Cork, Ireland'
+        choice3: 'Challenge Wanaka, New Zealand'
+        choice4: 'Norseman Xtreme, Norway'
+    }
+]
